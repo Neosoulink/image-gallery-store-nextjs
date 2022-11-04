c91e526dc350c261f03abf68a84752c9a87effad
Neosoulink <neodarksoulink@gmail.com>
2022-11-04 04:56:11 +0200

    feat: init firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { useEffect } from "react";
+
+// HELPER
+import FirebaseHelper from "../helpers/firebase";
 
 // STYLES
 import "../styles/main.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	useEffect(() => {
+		FirebaseHelper.init();
+
+		return () => {};
+	}, []);
+
+	return (
+		<>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
