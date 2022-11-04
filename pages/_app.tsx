81a3531a0739156f413124ca129ef3094149e2e8
Neosoulink <neodarksoulink@gmail.com>
2022-11-04 03:35:59 +0200

    feat: setup TailwindCss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/global.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+// STYLES
+import "../styles/main.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
